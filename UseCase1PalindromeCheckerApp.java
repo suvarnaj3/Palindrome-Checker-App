a26c4a17ba175fab223b842398084537fac70f4d
suvarnaj3 <suvarnajaiswal3@gmail.com>
2026-02-24 09:31:51 +0000

    UC1 IMPLEMENTED

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+
+
+ 
+public class UseCase1PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("   WELCOME TO PALINDROME CHECKER APP   ");
+        System.out.println("Version: 1.0");
+        System.out.println();
+        System.out.println("Application started successfully.");
+        System.out.println("Palindrome logic will be implemented in upcoming use cases.");
+
+    }
+}
